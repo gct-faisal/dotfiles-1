115c609bfa9a504ca7fe260f8957301db9621b52
Julien Bisconti <julien.bisconti@gmail.com>
2014-07-19 07:13:30 +0200

    adding eclim

--- export.zsh
+++ export.zsh
@@ -2,3 +2,4 @@ export PATH=$HOME/bin:/usr/local/bin:$PATH
 export LANG=en_US.UTF-8
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 export JRE_HOME=/usr/lib/jvm/java-8-oracle/jre
+export ECLIPSE_HOME=$HOME/bin/eclipse
