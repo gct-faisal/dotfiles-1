6e2de3c353b05c1fb454d3598d2fbaf8a22532c2
Julien Bisconti <julien.bisconti@gmail.com>
2014-07-19 19:29:43 +0200

    adding GOROOT and GOPATH

--- export.zsh
+++ export.zsh
@@ -1,5 +1,9 @@
-export PATH=$HOME/bin:/usr/local/bin:$PATH
 export LANG=en_US.UTF-8
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 export JRE_HOME=/usr/lib/jvm/java-8-oracle/jre
 export ECLIPSE_HOME=$HOME/bin/eclipse
+export GOROOT=$HOME/bin/golang
+export GOPATH=$HOME/code/gocode
+
+export PATH=$HOME/bin:/usr/local/bin:$PATH:$GOROOT/bin:$GOPATH/bin
+
